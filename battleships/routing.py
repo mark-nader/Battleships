1d6fcdc7eb03d443c7cb891114a2cb94d8eb8366
mark-nader <36566830+mark-nader@users.noreply.github.com>
2018-03-04 19:44:56 +0000

    Update routing.py

--- battleships/routing.py
+++ battleships/routing.py
@@ -1,4 +1,12 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.routing import route, route_class
+from channels.staticfiles import StaticFilesConsumer
+from game import consumers
+
+channel_routing = [
+	route_class(consumers.LobbyConsumer,  path=r"^/lobby/"),
+	route_class(consumers.GameConsumer,  path=r"^/game/(?P<game_id>\d+)/$")
+]
 
 application = ProtocolTypeRouter({})
