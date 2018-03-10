3030f6084b0e9472d3ce392d81eb53cd2d696825
mark-nader <36566830+mark-nader@users.noreply.github.com>
2018-03-10 02:09:46 +0000

    Update routing.py

--- battleships/routing.py
+++ battleships/routing.py
@@ -1,4 +1,13 @@
+from django.conf.urls import url
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from game import consumers
 
-application = ProtocolTypeRouter({})
+application = ProtocolTypeRouter({
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            url(r"^/lobby/", consumers.LobbyConsumer),
+            url(r"^/game/(?P<game_id>\d+)/$", consumers.GameConsumer),
+        ])
+    )
+})
